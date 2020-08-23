8138ad62a3fe1763e20e1732af2ed080e5d9136b
jozadaque <jozadaquesousa@gmail.com>
2020-08-22 23:16:31 -0300

    aprendizado basico de variaveis

---
+++ fundamentos/variaveis_1.dart
@@ -0,0 +1,16 @@
+main() {
+
+
+  //Exercicio de criação de variáveis
+
+  //Valores Literais
+  print(1 + 2 * 4 );
+
+  int a = 2;
+  print(a);
+  double b = 3.1314;
+  b=3.1415;
+  print(b);
+
+  print (a + b);
+}
---
+++ fundamentos/variaveis_2.dart
@@ -0,0 +1,15 @@
+main(){
+
+var a =2;
+var b = 4.56;
+var texto = "O valor da soma é: ";
+print(a + b);
+
+print(a.runtimeType);
+print(b.runtimeType);
+print(texto.runtimeType);
+
+
+print(a is String);
+print(a is int);
+}
